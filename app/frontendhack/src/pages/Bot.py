47247ff41fcd9ccfe6b79c486cacca927f6f62f3
djsammyjam <sami.lankiniemi@icloud.com>
2024-03-02 15:01:05 +0200

    Updated front functionalities

--- app/frontendhack/src/pages/Bot.py
+++ app/frontendhack/src/pages/Bot.py
@@ -1,34 +1,52 @@
 import time
 import streamlit as st
 
-# Basic setup
+#Basic setup
 st.set_page_config(page_title="AugEng", page_icon=":flash:")
-
-# Load
-progress_text = "Opening search. Please wait."
-my_bar = st.progress(0, text=progress_text)
-for percent_complete in range(100):
-    time.sleep(0.01)
-    my_bar.progress(percent_complete + 1, text=progress_text)
-time.sleep(0.09)
-my_bar.empty()
-
-# Chat messaging window
-with st.chat_message(name="AI"):
-    st.write("Answer is written here")
-
-question = st.chat_input(placeholder="Ask questions to find the text you need!", key=str, max_chars=500, disabled=False)
 
 # Sidebar and Selected focus area
 with st.sidebar:
-    st.image(
-        "https://www.sitowise.com/sites/default/files/styles/image_with_text_576x370_1x/public/2020-11/Sitowise_Haalarimerkit_Smart%20satama.png?itok=xMQFn05C",
-        width=100)
     st.title("RAGforce :rocket:")
     st.subheader("Augmented search for engineers")
     st.divider()
-
-add_selectbox = st.sidebar.selectbox(
+    add_selectbox = st.sidebar.selectbox(
     "Select Business Area",
-    ("Infrastructure", "Buildings", "Digital Solutions")
+    ("Infrastructure", "Buildings", "Digital Solutions")    
 )
+    on = st.toggle('CO2 calculation methods')
+    st.toggle('Legislation')
+    st.toggle('Design guidelines')
+    st.toggle('Technical specification')
+    st.toggle('QA guidelines')
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"], 
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        with st.spinner('Searching for answers...   '):    
+            time.sleep(2)
+        response = st.write_stream(stream)  
+    st.session_state.messages.append({"role": "assistant", "content": response})
+          
